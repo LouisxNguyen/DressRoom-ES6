315a52492cd7d0f80ccd358d38c6dc9e02d77f7a
Louis Nguyen <nammnguyenforwork@gmail.com>
2024-03-21 10:20:02 +0700

    update code

--- scr/controllers/index.js
+++ scr/controllers/index.js
@@ -17,7 +17,6 @@ const getTabList = () => {
                 getItemList(type)
             }
             window.checkType = checkType;
-            
         })
 }
 getTabList();
@@ -58,10 +57,37 @@ const renderUIItem = (data, type) => {
         <img class="w-50" src="../../assets/images/${type}/${item.imgSrc_jpg}" />
         <h4>${item.name}</h4>
         <p>${item.type}</p>
-        <button class="btn btn-outline-success">Thử đồ</button>
+        <button class="btn btn-outline-success" onclick="changeClo('${item.type}','${item.imgSrc_png}')">Thử đồ</button>
         </div>
         `
     })
     domID("tab-content").innerHTML = content;
 }
 
+const changeStyle = (type, width, height, img, bottom, right, scale, zindex) => {
+    domID(`${type}`).style = `width: ${width}px; height: ${height}px; background: url(../../assets/images/${type}/${img}); position: absolute; bottom: ${bottom}%; right: ${right}%; transform: scale(${scale}); z-index: ${zindex};`
+}
+const changeClo = (type, img) => {
+    if(type == 'necklaces'){
+        changeStyle(type, 500, 1000, img, -40, -3.5, 0.5, 4);
+    }
+    else if(type == 'hairstyle'){
+        changeStyle(type, 1000, 1000, img, -75, -57, 0.15, 4);
+    }
+    else if(type == 'topclothes'){
+        changeStyle(type, 500, 500, img, -9, -5, 0.5, 3);
+    }
+    else if(type == 'botclothes'){
+        changeStyle(type, 500, 1000, img, -30, -5, 0.5, 2);
+    }
+    else if(type == 'shoes'){
+        changeStyle(type, 500, 1000, img, -40, -3.5, 0.5, 1);
+    }
+    else if(type == 'handbags'){
+        changeStyle(type, 500, 1000, img, -37, -3.5, 0.5, 4);
+    }
+    else if(type == 'background'){
+        domID(`${type}`).style = `background-image: url(../../assets/images/${type}/${img})`
+    }
+}
+window.changeClo = changeClo;
